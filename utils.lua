6c737c34171f35918e7b65182530181bc915e05a
Wynn Greene <8239614+wynngreene@users.noreply.github.com>
2026-03-04 16:47:28 -0700

    Create utils.lua

---
+++ utils.lua
@@ -0,0 +1,23 @@
+-- utils.lua
+function aabb(a,b)
+ return not (
+  a.x+a.w < b.x or
+  b.x+b.w < a.x or
+  a.y+a.h < b.y or
+  b.y+b.h < a.y
+ )
+end
+
+function near(a,b,dist)
+ local ax=a.x+a.w/2
+ local ay=a.y+a.h/2
+ local bx=b.x+b.w/2
+ local by=b.y+b.h/2
+ return abs(ax-bx)<=dist and abs(ay-by)<=dist
+end
+
+function clamp(v,a,b)
+ if v<a then return a end
+ if v>b then return b end
+ return v
+end
